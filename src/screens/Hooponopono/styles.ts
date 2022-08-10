8167c50a6530d38b77b832a339c983e5aecfbfa9
gambarotto <dgcdeveloper@gmail.com>
2022-08-10 16:01:06 -0300

    fix

--- src/screens/Hooponopono/styles.ts
+++ src/screens/Hooponopono/styles.ts
@@ -1,11 +1,8 @@
 import { Dimensions, StyleSheet } from 'react-native';
-import styled, { css } from 'styled-components/native';
+import styled from 'styled-components/native';
 
 const HEIGHT_DEVICE = Dimensions.get('window').height;
 
-interface TextProps {
-  reduceFont: boolean;
-}
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -27,19 +24,4 @@ export const ContainerHooponopono = styled.View`
   position: absolute;
   width: 100%;
   bottom: ${`${HEIGHT_DEVICE / 3.5}px`};
-`;
-export const TextHooponopono = styled.Text<TextProps>`
-  ${(props) => css`
-    color: ${`${props.theme.colors.white}`};
-  `}
-  ${(props) =>
-    !props.reduceFont
-      ? css`
-          font-size: ${`${props.theme.fontSize.mainText} `};
-        `
-      : css`
-          font-size: 12px;
-        `}
-  font-family: ${({ theme }) => theme.fonts.bold};
-  text-align: center;
 `;
